d347e83dec994baa495160af4694cad19736ccc7
Paola Katherine <paolakatherine@pk.local>
2023-01-19 15:50:28 -0600

    add changes for read json

--- rpl_paola_katherine_appendix_A.py
+++ rpl_paola_katherine_appendix_A.py
@@ -13,22 +13,6 @@ import requests
 from datetime import datetime, timedelta
 
 
-# # #check connectivity
-# url_stops = f"https://api.winnipegtransit.com/v3/stops.json?lon={long}&lat={lat}&distance={distance}&api-key={API_KEY}"
-# response = requests.get(url_stops)
-# print(response)
-
-# API_KEY = "wX6qfnO8Yuxb4jkkJJRP"
-# url_test = f"https://api.winnipegtransit.com/v3/stops/10064/schedule?api-key={API_KEY}"
-# response = requests.get(url_test)
-# if response.status_code == 200:
-#     print(response.json())
-# else:
-#     print("Error from server: " + str(response.content))
-
-
-
-
 class BusWinnipeg():
 
     def __init__(self):
@@ -43,11 +27,10 @@ class BusWinnipeg():
         return start
     
     def transform_date_end(self):
-        end_no_format = datetime.now() + timedelta(minutes=5)
+        end_no_format = datetime.now() + timedelta(minutes=30)
         end = end_no_format.strftime('%Y-%m-%d{}%H:%M:%S').format("T")
         return  end
 
-# result:  key    name ( ex: 123  Portage Av) limit 5
     def fetch_stops(self, 
                     long, 
                     lat,
@@ -57,36 +40,50 @@ class BusWinnipeg():
         url_stop = self.url_base+ api_stop
         request_stop = requests.get(url_stop)
         json_stop = json.loads(request_stop.text)
-        print (json_stop)
 
-# result:  
+        print("Stops available", distance,"from the coodinates (",lat, long,"):" )
+        for i in json_stop['stops']:
+            print(i['key'], '  ', i['name'])
+
+# result: scheduled and estimated   
     def fetch_schedule(self,
                        bus_stop, 
                        ):
-        api_schedule = "stops/{}/schedule?start={}&end={}&api-key={}".format(bus_stop,self.start, self.end, self.api_key)
+        api_schedule = "stops/{}/schedule.json?start={}&end={}&api-key={}".format(bus_stop,self.start, self.end, self.api_key)
         url_schedule = self.url_base + api_schedule
-        print(url_schedule)
         request_schedule = requests.get(url_schedule)
+        schedule_json = json.loads(request_schedule.text)
+        item_list = ['scheduled', 'estimated'] 
         import ipdb;ipdb.set_trace()
-        # schedule_json = json.loads(request_schedule.text)
-        print (request_schedule)
+        for i in schedule_json['stop-schedule']:
+            print(i['scheduled'], '  ', i['estimated'])
 
 
 if __name__ == "__main__":
 
     option_user = int(input("Press 1 for stops available, Press 2 for Schedule  "))
+    
     bw = BusWinnipeg()
 
     if option_user == 1:
-        long = input("Enter the longitude: (example : -97.138)" )
-        lat = input("Enter the latitude: ( example(49.895)")
-        distance = input("Enter the distance: (example: 100, 200, 250)")
+        lat_long_input = int(input("Press 1 for random coordinates, Press 2 for input the coordinates "))
+        if lat_long_input == 1:
+            long = 49.895
+            lat = -97.138
+            distance = 200
+
+        elif lat_long_input == 2:
+            long = input("Enter the longitude: (example : -97.138)" )
+            lat = input("Enter the latitude: ( example(49.895)")
+            distance = input("Enter the distance: (example: 100, 200, 250)")
+
+        else:
+            print('Please enter a valid number')
         bw.fetch_stops (long, lat, distance)
+
     elif option_user == 2:
         bus_stop = input("Enter the  stop number:")
         bw.fetch_schedule(bus_stop)
+
     else:
         print("Please enter a valid number")
-
-
-    
